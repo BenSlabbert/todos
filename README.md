dd184e4dc35e8051748c66d76c087e106bb23160
Ben Slabbert <ben@hex.co.za>
2017-12-17 19:10:08 +0200

    updating readme reducing test data

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Simple Todo App in the MEAN stack
 
 Dummy data from: https://www.json-generator.com/
+
+API Test on Postman here: https://documenter.getpostman.com/view/1229795/collection/7LjBPRS
--- controllers/setupControllers.js
+++ controllers/setupControllers.js
@@ -21,48 +21,6 @@ module.exports = function ( app ) {
                 "todo": "in aliqua sunt culpa irure",
                 "isDone": false,
                 "hasAttachment": true
-            },
-            {
-                "username": "v",
-                "todo": "sit eiusmod adipisicing dolore et",
-                "isDone": false,
-                "hasAttachment": true
-            },
-            {
-                "username": "test",
-                "todo": "aliqua elit cillum dolore enim",
-                "isDone": false,
-                "hasAttachment": true
-            },
-            {
-                "username": "test",
-                "todo": "in aute ipsum eiusmod veniam",
-                "isDone": false,
-                "hasAttachment": true
-            },
-            {
-                "username": "test",
-                "todo": "aute proident sunt non Lorem",
-                "isDone": true,
-                "hasAttachment": true
-            },
-            {
-                "username": "test",
-                "todo": "amet exercitation anim et officia",
-                "isDone": true,
-                "hasAttachment": true
-            },
-            {
-                "username": "test",
-                "todo": "excepteur nulla Lorem cillum nostrud",
-                "isDone": true,
-                "hasAttachment": false
-            },
-            {
-                "username": "test",
-                "todo": "veniam velit exercitation ad dolore",
-                "isDone": true,
-                "hasAttachment": true
             }
         ];
         Todos.create( starterTodos, function ( err, results ) {
